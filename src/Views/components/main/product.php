0dec3b494d0fc2625ac495c182814197ff8a3757
Karina Demchenko <karina.demchenko.02@mail.ru>
2024-02-03 22:35:18 +0200

    add layout

---
+++ src/Views/components/main/product.php
@@ -0,0 +1,41 @@
+<li class="product__wrapper">
+    <div class="product__content">
+        <img class="product__images" src="images/productImages.png" alt="product">
+        <div class="product__bottom-content">
+            <div class="product__left-content">
+                <div class="product__details">
+                    <h2 class="product__heading">Корм Premier</h2>
+                    <p class="product__price">570 ₽</p>
+                </div>
+                <button class="product__buy">
+                    <i class="material-icons product__icon-buy">add_shopping_cart</i>
+                </button>
+            </div>
+            <div class="product__right-content">
+                <div class="product__right-done">
+                    <i class="material-icons product__done-icon">done</i>
+                </div>
+                <div class="product__details">
+                    <h2 class="product__heading product__heading-details">Корм Premier</h2>
+                    <p class="product__price product__price-details">Товар в корзине</p>
+                </div>
+                <div class="product__right-remove">
+                    <i class="material-icons product__remove-icon">clear</i>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="product__information">
+        <div class="product__container-icon">
+            <i class="product__icon material-icons">info_outline</i>
+        </div>
+        <div class="product__more-content">
+            <p class="product__info">Для взрослых кошек с нормальной активностью,
+                чувствительным пищеварением или склонных к аллергии.<br>
+                Свежее гипоаллергенное и питательное мясо ягненка и постной индейки благотворно влияют на пищеварительную систему кошки.
+                Благодаря гипоаллергенным ингредиентам, одному виду зерновых, отсутствию глютена, корм имеет отличную усвояемость,
+                высокую пищевую ценность и помогает оставаться вашему питомцу здоровым и жизнерадостным на протяжении долгих лет жизни.
+            </p>
+        </div>
+    </div>
+</li>
