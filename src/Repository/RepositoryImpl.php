481564052ee0bb31b0915cc52f824ffac02761c3
Suleymanov Artur <l0tus0rb@yandex.ru>
2024-02-06 14:58:36 +0200

    Change getDbConnection

--- src/Repository/RepositoryImpl.php
+++ src/Repository/RepositoryImpl.php
@@ -6,12 +6,7 @@ class RepositoryImpl
 {
 	public static function getResultSQLQuery(string $sql): \mysqli_result|bool
 	{
-		$connection = \Up\Util\Database\Connector::getInstance(
-			\Up\Util\Configuration::getInstance()->option('DB_HOST'),
-			\Up\Util\Configuration::getInstance()->option('DB_USER'),
-			\Up\Util\Configuration::getInstance()->option('DB_PASSWORD'),
-			\Up\Util\Configuration::getInstance()->option('DB_NAME')
-		)->getDbConnection();
+		$connection = \Up\Util\Database\Connector::getInstance()->getDbConnection();
 
 		$result = mysqli_query($connection, $sql);
 
