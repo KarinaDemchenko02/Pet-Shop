54ddc69fc51489e29e090a1610949ec203ef2325
Nikita Tikhomirov <nikita.t.18.09@gmail.com>
2024-02-17 02:05:37 +0200

    add http build query in pagination

--- src/View/components/main/pagination.php
+++ src/View/components/main/pagination.php
@@ -42,16 +42,20 @@ if ($countPage > 1)
 
 <div class="pagination">
 	<?php if ($activePage !== 1) { ?>
-		<a class="pagination__button-switch pagination__button_return" href="/?page=1">Первая страница</a>
-		<a class="pagination__button-switch pagination__button_next" href="<?="/?page=" . ($activePage- 1)?>">Назад</a>
+<!--		<a class="pagination__button-switch pagination__button_return" href="/?page=1">Первая страница</a>-->
+<!--		<a class="pagination__button-switch pagination__button_next" href="--><?php //="/?page=" . ($activePage- 1)?><!--">Назад</a>-->
+		<a class="pagination__button-switch pagination__button_return" href='/?<?= http_build_query(array_merge($_GET, ['page' => 1])) ?>'>Первая страница</a>
+		<a class="pagination__button-switch pagination__button_next" href='/?<?= http_build_query(array_merge($_GET, ['page' => $activePage- 1])) ?>'>Назад</a>
 	<?php } ?>
 	<?php for ($p = $start; $p <= $end; $p++) { ?>
-		<a class="pagination__button" href="<?= "/?page=" . $p ?>">
+		<a class="pagination__button" href='/?<?= http_build_query(array_merge($_GET, ['page' => $p])) ?>'>
 			<?= $p ?>
 		</a>
 	<?php } ?>
 	<?php if ($activePage !== $countPage) { ?>
-		<a class="pagination__button-switch pagination__button_next" href="<?="/?page=" . ($activePage + 1)?>">Вперед</a>
-		<a class="pagination__button-switch pagination__button_return" href="<?="/?page=" . $countPage?>">Последняя страница</a>
+		<a class="pagination__button-switch pagination__button_next" href='/?<?= http_build_query(array_merge($_GET, ['page' => $activePage + 1])) ?>'>Вперед</a>
+		<a class="pagination__button-switch pagination__button_return" href='/?<?= http_build_query(array_merge($_GET, ['page' => $countPage])) ?>'>Последняя страница</a>
+<!--		<a class="pagination__button-switch pagination__button_next" href="--><?php //="/?page=" . ($activePage + 1)?><!--">Вперед</a>-->
+<!--		<a class="pagination__button-switch pagination__button_return" href="--><?php //="/?page=" . $countPage?><!--">Последняя страница</a>-->
 	<?php } ?>
 </div>
