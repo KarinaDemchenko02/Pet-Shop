34fe4251269afcdc8bf04c5b646cc26db428e38e
Nikita Tikhomirov <nikita.t.18.09@gmail.com>
2024-02-17 02:06:22 +0200

    refactoring

--- src/View/components/main/pagination.php
+++ src/View/components/main/pagination.php
@@ -4,14 +4,14 @@ $countShowPages = 4;
 
 if (isset($_GET['page']))
 {
-	$activePage = (int) $_GET['page'];
+	$activePage = (int)$_GET['page'];
 }
 else
 {
 	$activePage = 1;
 }
 
-$countPage = (int) $this->getVariable('countPage');
+$countPage = (int)$this->getVariable('countPage');
 
 if ($countPage > 1)
 {
@@ -41,21 +41,33 @@ if ($countPage > 1)
 ?>
 
 <div class="pagination">
-	<?php if ($activePage !== 1) { ?>
-<!--		<a class="pagination__button-switch pagination__button_return" href="/?page=1">Первая страница</a>-->
-<!--		<a class="pagination__button-switch pagination__button_next" href="--><?php //="/?page=" . ($activePage- 1)?><!--">Назад</a>-->
-		<a class="pagination__button-switch pagination__button_return" href='/?<?= http_build_query(array_merge($_GET, ['page' => 1])) ?>'>Первая страница</a>
-		<a class="pagination__button-switch pagination__button_next" href='/?<?= http_build_query(array_merge($_GET, ['page' => $activePage- 1])) ?>'>Назад</a>
-	<?php } ?>
-	<?php for ($p = $start; $p <= $end; $p++) { ?>
+	<?php
+	if ($activePage !== 1) { ?>
+
+		<a class="pagination__button-switch pagination__button_return" href='/?<?= http_build_query(
+			array_merge($_GET, ['page' => 1])
+		) ?>'>Первая страница</a>
+		<a class="pagination__button-switch pagination__button_next" href='/?<?= http_build_query(
+			array_merge($_GET, ['page' => $activePage - 1])
+		) ?>'>Назад</a>
+	<?php
+	} ?>
+	<?php
+	for ($p = $start; $p <= $end; $p++) { ?>
 		<a class="pagination__button" href='/?<?= http_build_query(array_merge($_GET, ['page' => $p])) ?>'>
 			<?= $p ?>
 		</a>
-	<?php } ?>
-	<?php if ($activePage !== $countPage) { ?>
-		<a class="pagination__button-switch pagination__button_next" href='/?<?= http_build_query(array_merge($_GET, ['page' => $activePage + 1])) ?>'>Вперед</a>
-		<a class="pagination__button-switch pagination__button_return" href='/?<?= http_build_query(array_merge($_GET, ['page' => $countPage])) ?>'>Последняя страница</a>
-<!--		<a class="pagination__button-switch pagination__button_next" href="--><?php //="/?page=" . ($activePage + 1)?><!--">Вперед</a>-->
-<!--		<a class="pagination__button-switch pagination__button_return" href="--><?php //="/?page=" . $countPage?><!--">Последняя страница</a>-->
-	<?php } ?>
+	<?php
+	} ?>
+	<?php
+	if ($activePage !== $countPage) { ?>
+		<a class="pagination__button-switch pagination__button_next" href='/?<?= http_build_query(
+			array_merge($_GET, ['page' => $activePage + 1])
+		) ?>'>Вперед</a>
+		<a class="pagination__button-switch pagination__button_return" href='/?<?= http_build_query(
+			array_merge($_GET, ['page' => $countPage])
+		) ?>'>Последняя страница</a>
+
+	<?php
+	} ?>
 </div>
