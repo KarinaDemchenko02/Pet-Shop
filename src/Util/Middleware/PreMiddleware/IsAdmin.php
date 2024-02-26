20addd1ca3ad70865b4ef74e55df2048cc393d70
Артем Калиниченко <kalinichenko.artyom@gmail.com>
2024-02-26 17:25:58 +0200

    fixed authorization at admin panel

--- src/Util/Middleware/PreMiddleware/IsAdmin.php
+++ src/Util/Middleware/PreMiddleware/IsAdmin.php
@@ -2,6 +2,7 @@
 
 namespace Up\Util\Middleware\PreMiddleware;
 
+use Up\Auth\JwtService;
 use Up\Http\Request;
 use Up\Http\Response;
 use Up\Http\Status;
@@ -18,7 +19,7 @@ class IsAdmin implements PreMiddleware
 		{
 			return $next($request);
 		}
-
+		JwtService::deleteCookie('jwt');
 		return new Response(Status::SEE_OTHER, ['redirect' => '/admin/logIn/']);
 	}
 }
