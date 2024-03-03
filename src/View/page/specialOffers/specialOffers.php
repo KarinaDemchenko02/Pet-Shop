19b514e290fbe145ec43f49f2b36fe7fb218722a
Nikita Tikhomirov <nikita.t.18.09@gmail.com>
2024-03-03 20:30:22 +0200

    fix special offer

--- src/View/page/specialOffers/specialOffers.php
+++ src/View/page/specialOffers/specialOffers.php
@@ -1,3 +1,6 @@
+<?php
+$isLogin = $this->getVariable('isLogIn');
+?>
 <div class="main__container">
 	<section class="form">
 		<?php $this->getVariable('form')->display() ?>
@@ -13,3 +16,13 @@
 		</ul>
 	</section>
 </div>
+<script type="module">
+	import {Auth} from "/js/main/auth/auth.js";
+
+	const auth = new Auth({
+		attachToNodeId: 'form-auth',
+		login: <?= \Up\Util\Json::encode($isLogin) ?>,
+	})
+
+	auth.render();
+</script>
