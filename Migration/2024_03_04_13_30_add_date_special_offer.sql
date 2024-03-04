e87d3dd6071f1f85490ff445e8a760eff4272b51
Nikita Tikhomirov <nikita.t.18.09@gmail.com>
2024-03-04 13:54:21 +0200

    fix migration

--- Migration/2024_03_04_13_30_add_date_special_offer.sql
+++ Migration/2024_03_04_13_30_add_date_special_offer.sql
@@ -4,7 +4,7 @@ alter table up_special_offer
 alter table up_special_offer
 	add end_date date null;
 
-UPDATE eshop.up_special_offer t SET t.start_date = '2024-03-01', t.end_date = '2024-03-31' WHERE t.id = 1;
+UPDATE up_special_offer t SET t.start_date = '2024-03-01', t.end_date = '2024-03-31' WHERE t.id = 1;
 
 alter table up_special_offer
 	modify start_date date not null;
